d1ad04c74fa9194e81fe0ad7916ac34a9b5a8d29
Yedi20 <20185857@itla.edu.do.com>
2021-06-17 00:25:50 -0400

    Parte 1, ya que tambien le hare su parte 2

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+require 'phpqrcode/qrlib.php';
+
+$dir ='temp/';
+if (!file_exists($dir))
+mkdir($dir);
+
+$filename =$dir.'test.png';
+
+$tamanio = 10;
+$level ='M';
+$frameSize =3;
+$contenido ='Factura';
+
+QRcode::png($contenido, $filename, $level, $tamanio, $frameSize);
+
+echo '<img src="'.$filename.'"/>';
+
+?>
